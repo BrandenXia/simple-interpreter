a9d0afec502a8a1b2d25b2c143df14842a5406f2
BrandenXia <brandenxia@163.com>
2023-11-07 19:22:47 -0800

    docs(parser): Add comments

--- include/parser/ast.h
+++ include/parser/ast.h
@@ -3,6 +3,15 @@
 
 #include "lexer/token.h"
 
+/*
+ * AST node types:
+ * - AST_NODE_TYPE_PROGRAM: program
+ * - AST_NODE_TYPE_STMT: statement
+ * - AST_NODE_TYPE_EXPR: expression
+ * - AST_NODE_TYPE_VAR: variable
+ * - AST_NODE_TYPE_CONST: constant
+ * - AST_NODE_TYPE_OP: operator
+ */
 typedef enum {
     AST_NODE_TYPE_PROGRAM,
     AST_NODE_TYPE_STMT,
@@ -12,6 +21,14 @@ typedef enum {
     AST_NODE_TYPE_OP
 } ASTNodeType;
 
+/*
+ * AST node:
+ * - type: node type
+ * - token: tokens
+ * - token_count: number of tokens
+ * - children: children nodes
+ * - child_count: number of children nodes
+ */
 typedef struct ASTNode {
     ASTNodeType type;
     Token *token;
@@ -20,12 +37,24 @@ typedef struct ASTNode {
     int child_count;
 } ASTNode;
 
+/*
+ * Initialize an AST node.
+ */
 void initializeASTNode(ASTNode **dst, ASTNodeType type);
 
+/*
+ * Free an AST node.
+ */
 void freeASTNode(ASTNode *node);
 
+/*
+ * Add a child node to the parent node.
+ */
 void addASTNodeChild(ASTNode *parent, ASTNode *child);
 
+/*
+ * Add a token to the AST node.
+ */
 void addASTNodeToken(ASTNode *node, Token *token);
 
 #endif //SIMPLE_INTERPRETER_AST_H
