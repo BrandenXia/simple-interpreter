f9b1669c4864ab09ff5c2d8be98f3492975947f9
BrandenXia <brandenxia@163.com>
2023-11-18 22:44:31 -0800

    fix(cli): Fix not exit when type exit

--- src/main.c
+++ src/main.c
@@ -58,6 +58,9 @@ int main(int argc, char *argv[]) {
         printf(">>> ");
 
         while (getline(&line, &len, stdin) != -1) {
+            if (strcmp(line, "exit\n") == 0) {
+                break;
+            }
 
             run(fmemopen(line, strlen(line), "r"), cli->flags[TOKEN_FLAG], cli->flags[AST_FLAG], cli->flags[VERBOSE_FLAG]);
 
